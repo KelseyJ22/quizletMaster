af09ff2d3fc47ba9712ff488595949ff55f2de17
Seokho Hong <seokho_hong@yahoo.com>
2013-11-16 16:29:05 -0800

    fixing everything

--- src/database/SQLUtils.java
+++ src/database/SQLUtils.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package database;
 
 public class SQLUtils 
@@ -11,17 +10,3 @@ public class SQLUtils
 		return "'"+s+"'";
 	}
 }
-=======
-package database;
-
-public class SQLUtils 
-{
-	/** Puts single quotes around the string, and replaces quotes with the sql version of the single quote */
-	public static String format(String s)
-	{
-		s.replace("'", "''");
-		s.replace("\\", "\\\\");
-		return "'"+s+"'";
-	}
-}
->>>>>>> upstream/master
