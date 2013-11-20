a86db51117d8d1c5951b9a4dcf0c7be1adb7fd27
Marie Feng <marie.feng@gmail.com>
2013-11-20 00:46:34 -0800

    Added demonstration quiz and sign on page

--- src/login/AccountManager.java
+++ src/login/AccountManager.java
@@ -3,24 +3,24 @@ package login;
 import java.util.*;
 
 public class AccountManager {
-    public static final String ATTRIBUTE_NAME = "Account Manager";
-    private HashMap<String, String> db;
+	public static final String ATTRIBUTE_NAME = "Account Manager";
+	private HashMap<String, String> db;
 
-    public AccountManager() {
-        db = new HashMap<String, String>();
-        createNewAccount("Patrick", "1234");
-        createNewAccount("Molly", "FloPup");
-    }
+	public AccountManager() {
+		db = new HashMap<String, String>();
+		createNewAccount("Patrick", "1234");
+		createNewAccount("Molly", "FloPup");
+	}
 
-    public boolean isUsernameTaken(String username) {
-        return db.containsKey(username);
-    }
+	public boolean isUsernameTaken(String username) {
+		return db.containsKey(username);
+	}
 
-    public boolean isCorrectPassword(String username, String password) {
-        return isUsernameTaken(username) && db.get(username).equals(password);
-    }
+	public boolean isCorrectPassword(String username, String password) {
+		return isUsernameTaken(username) && db.get(username).equals(password);
+	}
 
-    public void createNewAccount(String username, String password) {
-        db.put(username, password);
-    }
+	public void createNewAccount(String username, String password) {
+		db.put(username, password);
+	}
 }
